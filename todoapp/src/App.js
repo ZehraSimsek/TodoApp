1dad5e0e23c7cf71112ac3f2393ddb4babf6f177
ZehraSimsek <125045707+ZehraSimsek@users.noreply.github.com>
2023-09-13 02:11:22 +0300

    some actions were taken

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,14 +1,45 @@
 import './App.css';
-import {Table , Row} from 'reactstrap'
+import { Table, Row } from 'reactstrap'
+import { useState } from 'react';
 
 function App() {
+
+  const [title, setTitle] = useState([]);
+  const [note, setNote] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setTitle([...title, note]);
+    setNote("");
+  };
+
+  const onChangeText = (e) => {
+    setNote(e.target.value);
+  };
+
   return (
     <>
       <h1>TODO</h1>
       <Table>
-          <Row>
-            Lets Check
-          </Row>
+        <Row>
+          <form onSubmit={onSubmit}>
+            <input className='input' value={note} placeholder='What`s in your mind?' onChange={onChangeText} />
+            <button onClick={onSubmit}>+</button>
+          </form>
+        </Row>
+
+        <Row>
+          {
+            title.map((title, index) => {
+              return (
+                <li key={index}>
+                  <input type="checkbox" />
+                  <span>{title}</span>
+                </li>
+              )
+            })
+          }
+        </Row>
       </Table>
 
     </>
